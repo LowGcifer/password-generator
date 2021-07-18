07e8c32f8ea24a034258c5eae3fb26d796e098c6
LowGcifer <LowGcifer@gmail.com>
2021-07-18 01:50:06 -0400

    Added all prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+function generateCharacterAmountRange() {
   var characterAmountRange = prompt(
     "How many characters do you want in the password, between 8 and 128."
   );
-  if (characterAmountRange >= 1 && characterAmountRange <= 128) {
+  if (characterAmountRange >= 8 && characterAmountRange <= 128) {
   } else {
     alert("Incorrect input!");
-    input = generatePassword();
+    input = generateCharacterAmountRange();
   }
+}
 
+function generateSpecialCharacters() {
   var specialCharacters = prompt(
     "Do you want to include special characters? Y/N"
   ).toUpperCase();
-  if (
-    specialCharacters === "Y" ||
-    speicalCharacters === "N" ||
-    specialCharacters === "Yes" ||
-    specialCharacters === "No"
-  ) {
+  if (specialCharacters === "Y" || specialCharacters === "N") {
   } else {
     alert("Incorrect input!");
-    input = generatePassword();
+    input = generateSpecialCharacters();
   }
 }
+
+function generateLowerCase() {
+  var lowerCase = prompt(
+    "Do you want to include lowercase letters? Y/N"
+  ).toUpperCase();
+  if (lowerCase === "Y" || lowerCase === "N") {
+  } else {
+    alert("Incorrect input!");
+    input = generateLowerCase();
+  }
+}
+
+function generateUpperCase() {
+  var upperCase = prompt(
+    "Do you want to include uppercase letters? Y/N"
+  ).toUpperCase();
+  if (upperCase === "Y" || upperCase === "N") {
+  } else {
+    alert("Incorrect input!");
+    input = generateUpperCase();
+  }
+}
+
+function generatePassword() {
+  generateCharacterAmountRange();
+  generateSpecialCharacters();
+  generateLowerCase();
+  generateUpperCase();
+}
+
+var characterAmountRange = generateCharacterAmountRange();
+var upperCase = generateUpperCase();
+var lowerCase = generateLowerCase();
+var specialCharacters = generateSpecialCharacters();
 
 // Write password to the #password input
 function writePassword() {
